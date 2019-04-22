5a42374be8e81939efc1577e4531972d27db9aca
Hai Phan <hai@haiphan.design>
2019-04-22 16:20:12 +0700

    1.0.6
    ios fix

--- js/main.js
+++ js/main.js
@@ -10,6 +10,23 @@ $(window).scroll(function () {
     }
 });
 
+//uses document because document will be topmost level in bubbling
+$(document).on('touchmove', function (e) {
+    e.preventDefault();
+});
+//uses body because jquery on events are called off of the element they are
+//added to, so bubbling would not work if we used document instead.
+$('body').on('touchstart', '.scrollable', function (e) {
+    if (e.currentTarget.scrollTop === 0) {
+        e.currentTarget.scrollTop = 1;
+    } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+        e.currentTarget.scrollTop -= 1;
+    }
+});
+//prevents preventDefault from being called on document if it sees a scrollable div
+$('body').on('touchmove', '.scrollable', function (e) {
+    e.stopPropagation();
+});
 
 // Transform Hamburgers Menu
 
@@ -27,7 +44,9 @@ $('.game-carousel').flickity({
 });
 
 $(function () {
-    $(".faq-accordion").accordion();
+    $(".faq-accordion").accordion({
+        collapsible: true
+    });
 });
 
 $(document).ready(function () {
